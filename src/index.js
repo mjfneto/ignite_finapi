7f3100e9e8738439d806078cd186e63fdef605e2
mjfneto <mjfneto@gmail.com>
2021-12-16 14:43:32 -0300

    POST method for account creation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+/**
+ * cpf ‒ string
+ * name ‒ string
+ * id ‒ uuid
+ * statement ‒ array
+ */
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  const account = {
+    cpf,
+    name,
+    id,
+    statement: [],
+  };
+
+  customers.push(account);
+
+  console.log(customers);
+
+  return res.status(201).send();
+});
 
 app.listen(3333);
